9b476264904e9376fdef5a80d73f7c90d43b325b
modify961@126.com <li809731496!!>
2024-05-12 21:32:36 +0800

    移除非必要文件

--- loadmodel/downModel.py
+++
@@ -1,6 +0,0 @@
-import torch
-from modelscope import snapshot_download, AutoModel, AutoTokenizer
-import os
-
-# 从modelscope下载-Llama-3-8B-Instruct的模型
-model_dir = snapshot_download('LLM-Research/Meta-Llama-3-8B-Instruct', cache_dir='E:/models', revision='master')
--- loadmodel/downcodeqwn.py
+++ loadmodel/downcodeqwn.py
@@ -4,3 +4,4 @@ import os
 
 # 从modelscope下载-CodeQwen1.5-7B-Chat的模型
 model_dir = snapshot_download('Qwen/CodeQwen1.5-7B-Chat', cache_dir='E:/models', revision='master')
+# model_dir = snapshot_download('LLM-Research/Meta-Llama-3-8B-Instruct', cache_dir='/root/autodl-tmp', revision='master')
